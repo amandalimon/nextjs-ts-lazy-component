fb83b48097959cfa44cdaee9c79320288d2a1775
Amanda Limón <amandalimon@outlook.com>
2024-02-13 13:10:49 -0600

    types for events and listener callbacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,31 @@
 'use client';
 import { useState } from 'react'
+import { MouseEventHandler } from 'react';
 import { RandomCat } from "./components/RandomCat";
 
-type ImageItems = { id: string, url: string }
+type ImageItem = { id: string, url: string }
 
 const generateId = () => Math.random().toString(36).substr(2, 9)
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItems>>([
-    { id: generateId(), url: `https://cataas.com/cat?${generateId()}` },
-    { id: generateId(), url: `https://cataas.com/cat?${generateId()}` },
-    { id: generateId(), url: `https://cataas.com/cat?${generateId()}` },
-    { id: generateId(), url: `https://cataas.com/cat?${generateId()}` },
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewCat: MouseEventHandler<HTMLButtonElement> = () => {
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://cataas.com/cat?${generateId()}`
+    }
+    setImages([
+      ...images,
+      newImageItem
+    ]);
+  }
 
   return (
     <main>
       <div className="m-9">
-        <h1 className="text-3xl font-bold">una flor para otra flor</h1>
-
+        <h1 className="text-3xl font-bold">Random cats!</h1>
+        <button onClick={addNewCat}>Add a random cat</button>
         {images.map(({ id, url }) => (
           <div className='my-9' key={id}>
             <RandomCat image={url} />
